195807aac2e38de66c12fb5745f86632689628ba
LeeJongHyun <leejh8848@hsociety.co.kr>
2019-03-14 11:46:12 +0900

    LiveData 사용법(1.개념편)

--- app/src/main/java/com/hyun/android/livedatasample/MainActivity.kt
+++ app/src/main/java/com/hyun/android/livedatasample/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.hyun.android.livedatasample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
